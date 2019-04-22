bf2021388071b2f11607fcbe4fff977d4ee16a77
huangxingguang <huangxingguang@marsfood.cn>
2019-04-22 13:39:50 +0800

    修复前进后退问题

--- src/com/starxg/browserfx/JavaFxBrowser.java
+++ src/com/starxg/browserfx/JavaFxBrowser.java
@@ -1,9 +1,8 @@
 package com.starxg.browserfx;
 
 import javafx.application.Platform;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker.State;
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -20,72 +19,73 @@ import java.util.function.Consumer;
  * @date 2019-04-21 13:52
  */
 public class JavaFxBrowser implements BrowserView {
-    private WebView browser;
-    private WebEngine webEngine;
-    private JFXPanel jfxPanel;
-    private int index;
+	private WebView browser;
+	private WebEngine webEngine;
+	private JFXPanel jfxPanel;
+	private int index;
 
-    public JavaFxBrowser() {
-        jfxPanel = new JFXPanel();
-        Platform.setImplicitExit(false);
-        Platform.runLater(() -> {
-            browser = new WebView();
-            webEngine = browser.getEngine();
-            BorderPane borderPane = new BorderPane();
-            borderPane.setCenter(browser);
-            Scene scene = new Scene(borderPane);
-            jfxPanel.setScene(scene);
-            webEngine.getHistory().getEntries().addListener((ListChangeListener<WebHistory.Entry>) c -> {
-                c.next();
-                index = c.getAddedSize();
-            });
+	public JavaFxBrowser() {
+		jfxPanel = new JFXPanel();
+		Platform.setImplicitExit(false);
+		Platform.runLater(() -> {
+			browser = new WebView();
+			webEngine = browser.getEngine();
+			BorderPane borderPane = new BorderPane();
+			borderPane.setCenter(browser);
+			Scene scene = new Scene(borderPane);
+			jfxPanel.setScene(scene);
+			webEngine.getHistory().getEntries().addListener((ListChangeListener<WebHistory.Entry>) c -> {
+				c.next();
+				index = c.getAddedSize();
+			});
 
-        });
-    }
+		});
+	}
 
-    @Override
-    public JComponent getBrowser() {
-        return jfxPanel;
-    }
+	@Override
+	public JComponent getBrowser() {
+		return jfxPanel;
+	}
 
-    @Override
-    public void load(String url) {
-        Platform.runLater(() -> webEngine.load(url));
-    }
+	@Override
+	public void load(String url) {
+		Platform.runLater(() -> webEngine.load(url));
+	}
 
-    @Override
-    public void urlChange(Consumer<String> consumer) {
-        Platform.runLater(() -> webEngine.getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> consumer.accept(webEngine.getLocation())));
-    }
+	@Override
+	public void urlChange(Consumer<String> consumer) {
+		Platform.runLater(() -> webEngine.getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> consumer.accept(webEngine.getLocation())));
+	}
 
-    @Override
-    public void back() {
-        Platform.runLater(() -> {
-            if (isPrev()) {
-                index++;
-                webEngine.getHistory().go(-1);
-            }
-        });
+	@Override
+	public void back() {
+		//https://stackoverflow.com/questions/18928333/how-to-program-back-and-forward-buttons-in-javafx-with-webview-and-webengine
+		final WebHistory history = webEngine.getHistory();
+		ObservableList<WebHistory.Entry> entryList = history.getEntries();
+		int currentIndex = history.getCurrentIndex();
+		Platform.runLater(() -> history.go(entryList.size() > 1 && currentIndex > 0 ? -1 : 0));
+	}
 
-    }
+	@Override
+	public void forward() {
+		//https://stackoverflow.com/questions/18928333/how-to-program-back-and-forward-buttons-in-javafx-with-webview-and-webengine
+		final WebHistory history = webEngine.getHistory();
+		ObservableList<WebHistory.Entry> entryList = history.getEntries();
+		int currentIndex = history.getCurrentIndex();
+		Platform.runLater(() -> history.go(entryList.size() > 1 && currentIndex < entryList.size() - 1 ? 1 : 0));
+	}
 
-    @Override
-    public void forward() {
-        Platform.runLater(() -> {
-            if (isNext()) {
-                index--;
-                webEngine.getHistory().go(1);
-            }
-        });
-    }
+	@Override
+	public boolean isNext() {
+		final WebHistory history = webEngine.getHistory();
+		ObservableList<WebHistory.Entry> entryList = history.getEntries();
+		return entryList.size() > 1 && history.getCurrentIndex() < entryList.size() - 1;
+	}
 
-    @Override
-    public boolean isNext() {
-        return webEngine.getHistory().getCurrentIndex() < (index - 1);
-    }
-
-    @Override
-    public boolean isPrev() {
-        return webEngine.getHistory().getCurrentIndex() > 0;
-    }
+	@Override
+	public boolean isPrev() {
+		final WebHistory history = webEngine.getHistory();
+		ObservableList<WebHistory.Entry> entryList = history.getEntries();
+		return entryList.size() > 1 && history.getCurrentIndex() > 0;
+	}
 }
